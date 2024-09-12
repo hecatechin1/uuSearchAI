f4901c11b16ad4161b7f10a69f2748eb054dc265
HecateChin <wallace0079@foxmail.com>
2024-09-12 17:06:18 +0800

    剪切板删除\n

--- src/routes/framed/+page.svelte
+++ src/routes/framed/+page.svelte
@@ -168,8 +168,8 @@
     let linebreakk = localStorage.getItem("linebreakkey") || "Shift+Enter";
 
 
-    let storedMessages = localStorage.getItem("search_messages");
-    // let storedMessages = null; // 24-9-10 搜索结果-AI搜索页面不再需要历史记录
+    // let storedMessages = localStorage.getItem("search_messages");
+    let storedMessages = null; // 24-9-10 搜索结果-AI搜索页面不再需要历史记录
 
 
 

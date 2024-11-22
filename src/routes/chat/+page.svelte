ed3b45567d7ec0d0ce5a38344ca664da5b24879a
HecateChin <wallace0079@foxmail.com>
2024-11-22 12:48:50 +0800

    //设置各种判断布尔值

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import ChatSidebar from '../../lib/ChatSidebar.svelte';
     import ChatMain from '../../lib/ChatMain.svelte';
-  
+
     // 状态管理
     let selectedChatId = "1";
   
@@ -13,7 +13,6 @@
     <div class="flex h-screen">
         <!-- 侧边栏 -->
         <ChatSidebar on:selectChat={handleChatSelection} />
-      
         <!-- 聊天主界面 -->
         <ChatMain selectedChatId={selectedChatId} />
     </div> 

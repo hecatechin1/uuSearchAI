9b6ef37fd25f478245755bc2963f4ba0bb977fc3
HecateChin <wallace0079@foxmail.com>
2025-01-24 14:10:32 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,11 @@ export default defineConfig({
 	 server: {
 	   host: '0.0.0.0', // 监听所有网络接口
 	   port: 5173,       // 你想要的端口
-	 }
+	   allowedHosts: [
+		'www.uugpt.com',  // 将此域名添加到允许的列表中
+		'localhost',      // 也可以添加其他你信任的域名
+		'127.0.0.1'
+	  ]
+	 },
+
 });

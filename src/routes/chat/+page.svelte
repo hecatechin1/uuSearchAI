5596946802d00165addbaa0de12449e70d2fd793
YueRay <wallace0079@foxmail.com>
2025-02-20 23:11:42 +0800

    1

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -112,9 +112,30 @@
   {#if showSettings}
     <UserSettings on:close-settings={()=>{showSettings = false}}/>
   {/if}
+{:else}
+<div class="flex h-screen">
+  <!-- Sidebar Skeleton -->
+  <aside class="sidebar-skeleton z-[21] flex shrink-0 overflow-x-hidden max-md:!w-0 w-64 flex-col h-full bg-grey-700">
+    <div class="header skeleton"></div>
+    <div class="chat-item skeleton"></div>
+    <div class="chat-item skeleton"></div>
+    <div class="chat-item skeleton"></div>
+    <div class="chat-item skeleton"></div>
+    <div class="message-loader"></div>
+  </aside>
+
+  <!-- Main Chat Skeleton -->
+  <div class="main-chat-skeleton">
+    <div class="message skeleton mb-5 message-flex-end"></div>
+    <div class="message skeleton"></div>
+    <div class="message large skeleton"></div>
+    <div class="message skeleton"></div>
+  </div>
+</div>
 {/if}
 
 <style>
+  @import '../../styles/skeleton.css';
   :global(html, body) {
     height: 100%;
   }
